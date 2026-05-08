42d2cbdeba528dbb2e9be81659f61607be91e1ea
omd-khaivd-m <khaivd@onemount.com>
2026-05-08 12:16:24 +0700

    config

--- BookingAdmin.Web/Program.cs
+++ BookingAdmin.Web/Program.cs
@@ -36,8 +36,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseRouting();
 app.UseSession();
 app.UseAuthentication();
