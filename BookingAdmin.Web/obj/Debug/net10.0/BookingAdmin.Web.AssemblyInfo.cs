c55e21daf8af201019b06014fcb5a0a3c7bf1f44
omd-khaivd-m <khaivd@onemount.com>
2026-05-13 15:38:50 +0700

    UI phòng, fix cột đầu, hàng đầu

--- BookingAdmin.Web/obj/Debug/net10.0/BookingAdmin.Web.AssemblyInfo.cs
+++ BookingAdmin.Web/obj/Debug/net10.0/BookingAdmin.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingAdmin.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6ee6ef35f090e7bff4b3e649d27adac7832dcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48df4311bc6d7f1412641cb05d8c18cd9badbb58")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingAdmin.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingAdmin.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
